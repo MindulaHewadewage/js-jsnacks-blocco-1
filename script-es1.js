52d046ed91261504e9afa9b9313d83af0e17dee5
MindulaHewadewage <mindufernando@gmail.com>
2022-12-13 16:24:25 +0100

    provando a stampre la somma

--- script-es1.js
+++ script-es1.js
@@ -1,7 +1,7 @@
 // Dichiaro glie elementi 
 const targetElement = document.getElementById('target');
 const userNumbers = document.getElementById('user-numbers')
-const sum = document = document.getElementById('sum');
+let sum = document = document.getElementById('sum');
 
 let numbers;
 // Genero il ciclo
@@ -9,7 +9,9 @@ for(let i = 0 ; i < 10 ; i++ ) {
     let numbers = prompt('Scegli un numero da sommare');
     // stampo i numeri nella console
     console.log(numbers);
-    // stampo i numeri nel dom
+
+    // faccio la somma
+    sum = numbers[i];
 }
 
-
+console.log('La somma è :' + sum);
