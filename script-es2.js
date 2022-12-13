fa8dfb1945f557cf19343c6604ada8a707ff158f
MindulaHewadewage <mindufernando@gmail.com>
2022-12-13 16:57:13 +0100

    aggancio event listener es 2

---
+++ script-es2.js
@@ -0,0 +1,11 @@
+// creo lista degli ospiti
+const guests = ['Pippo', 'Ciccio', 'Marco', 'Lorenz0' ,'Fabio'];
+
+// Dichiaro i documenti
+const guestName = document.getElementById('guest-name');
+const button = document.getElementById('button');
+
+// aggancio event listener al bottone
+button.addEventListener('click' , function(){
+    
+})
