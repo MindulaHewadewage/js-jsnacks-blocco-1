33fa693c6dac9be48f82cfe8baf486d3c398490d
MindulaHewadewage <mindufernando@gmail.com>
2022-12-13 15:39:56 +0100

    creato prompt

--- script-es1.js
+++ script-es1.js
@@ -1,4 +1,15 @@
-console.log ('JS OK')
-
+// Dichiaro glie elementi 
 const targetElement = document.getElementById('target');
+const userNumbers = document.getElementById('user-numbers')
 const sum = document = document.getElementById('sum');
+
+let numbers;
+// Genero il ciclo
+for(let i = 0 ; i < 10 ; i++ ) {
+    let numbers = prompt('Scegli un numero da sommare');
+    // stampo i numeri nella console
+    console.log(numbers);
+    // stampo i numeri nel dom
+}
+
+
