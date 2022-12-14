757c3c86b4405560bfa9de3222d00f4b323306b9
MindulaHewadewage <mindufernando@gmail.com>
2022-12-14 09:30:58 +0100

    struttura es 2

--- script-es2.js
+++ script-es2.js
@@ -2,10 +2,25 @@
 const guests = ['Pippo', 'Ciccio', 'Marco', 'Lorenz0' ,'Fabio'];
 
 // Dichiaro i documenti
-const guestName = document.getElementById('guest-name');
+const guestFieldElement = document.getElementById('guest-name');
 const button = document.getElementById('button');
+const result = document.getElementById('result');
+
+
+let validInvite = false;
 
 // aggancio event listener al bottone
 button.addEventListener('click' , function(){
-    
+    // Raccolgo il nome dall'input
+    guestName = guestFieldElement.value.trim();
+    console.log(guestName);
+    for (let i = 0; i < guests.length; i++) {
+
+
+        if (guestName === guests){
+        }
+    }
+
+
+    result.innerText = validInvite ? 'Ospite invitato' : 'Ospite non invitato';
 })
